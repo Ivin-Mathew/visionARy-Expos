08f1197efb12fa3263161ac0997a90b89df1dd42
Ivin <ivinmk2410@hotmail.com>
2024-06-19 22:49:03 +0530

    Red color text rendered

--- App.tsx
+++ App.tsx
@@ -1,56 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
 import {
   ViroARScene,
   ViroARSceneNavigator,
   ViroText,
   ViroTrackingReason,
   ViroTrackingStateConstants,
-} from "@reactvision/react-viro";
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+  ViroBox,
+} from '@reactvision/react-viro';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState("Initializing AR...");
-
-  function onInitialized(state: any, reason: ViroTrackingReason) {
-    console.log("onInitialized", state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText("Welcome to the world of AR");
-    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      // Handle loss of tracking
-    }
-  }
-
+const InitialScene = ()=>{
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
+    <ViroARScene>
       <ViroText
-        text={text}
-        scale={[0.2, 0.2, 0.2]}
-        position={[0, 0, -2]}
-        style={styles.helloWorldTextStyle}
+        text={'Hello world!'}
+        position={[0,0,-1]} //position of text
+        style={{fontSize: 30, fontFamily:'Arial',color:'red'}}
       />
     </ViroARScene>
-  );
-};
+  )
+}
+
 
 export default () => {
   return (
     <ViroARSceneNavigator
-      autofocus={true}
       initialScene={{
-        scene: HelloWorldSceneAR,
+        scene:InitialScene
       }}
-      style={styles.f1}
+      style={{flex:1}}
     />
   );
 };
 
 var styles = StyleSheet.create({
-  f1: { flex: 1 },
-  helloWorldTextStyle: {
-    fontFamily: "Arial",
-    fontSize: 90,
-    color: "#ffffff",
-    textAlignVertical: "center",
-    textAlign: "center",
-  },
+
 });
