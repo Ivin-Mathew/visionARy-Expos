da2bfe6df7bd35f237725181738600c78008e6c4
Ivin <ivinmk2410@hotmail.com>
2024-06-19 23:18:05 +0530

    rotating 3d box with texture

--- App.tsx
+++ App.tsx
@@ -7,28 +7,54 @@ import {
   ViroTrackingReason,
   ViroTrackingStateConstants,
   ViroBox,
+  ViroMaterials,
+  ViroAnimations,
 } from '@reactvision/react-viro';
 import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 const InitialScene = ()=>{
+
+  ViroMaterials.createMaterials({
+    dirt:{
+      diffuseTexture:require('./assets/dirt.jpeg'),
+    },
+  });
+
+  ViroAnimations.registerAnimations({
+    rotate:{
+      duration:2500,
+      properties:{
+        rotateY:'+=90',
+      },
+    },
+  });
+
   return (
     <ViroARScene>
-      <ViroText
+      {/* <ViroText
         text={'Hello world!'}
         position={[0,0,-1]} //position of text
         style={{fontSize: 30, fontFamily:'Arial',color:'red'}}
+      /> */}
+
+      <ViroBox
+      height={2} length={2} width={2}
+      scale={[0.2,0.2,0.2]}
+      position={[0,-1,-1]}
+      materials={['dirt']}
+      animation={{name:'rotate', loop:true, run:true}}
       />
     </ViroARScene>
-  )
-}
+  );
+};
 
 
 export default () => {
   return (
     <ViroARSceneNavigator
       initialScene={{
-        scene:InitialScene
+        scene:InitialScene,
       }}
       style={{flex:1}}
     />
